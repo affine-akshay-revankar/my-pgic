9cf915efc3bdc569a5a36119f98d2d46017dccee
lionel almeida <lionel.almeida@sonicautomotive.com>
2020-05-19 15:25:22 +0530

    initial API integration with data pos uploaf

--- src/app/home/components/ins-pos/ins-pos.component.ts
+++ src/app/home/components/ins-pos/ins-pos.component.ts
@@ -15,17 +15,95 @@ export class InsPosComponent implements OnInit {
   path:boolean = false;
   success:boolean = false;
   chooseFile:boolean = true;
-  array=[
-
-    {id:1,path:"./assets/Tide/Tide_1.png",name:"Tide"},
-    {id:2,path:"./assets/Tide/Tide_3.jpg",name:"Tide"},
-    {id:3,path:"../../../../assets/Tide/tide3.png",name:"Tide"},
-    {id:4,path:"../../../../assets/Tide/tide4.jpg",name:"Tide"},
-    {id:5,path:"../../../../assets/Tide/tide5.png",name:"Tide"},
-    {id:6,path:"../../../../assets/Head & Sholder/H1.jpg",name:"Head & Holders"},
-    {id:7,path:"../../../../assets/Head & Sholder/H2.jpg",name:"Head & Holders"},
-    {id:8,path:"../../../../assets/Head & Sholder/H3.jpg",name:"Head & Holders"},
-  ]
+  imagesList = [
+    {
+      "id":1,
+      "path":"./assets/Tide/Tide_1.png",
+      "name":"Tide"
+    },
+    {
+      "id":2,
+      "path":"./assets/Tide/Tide_2.jpg",
+      "name":"Tide"
+    },
+    {
+      "id":3,
+      "path":"./assets/Tide/Tide_3.jpg",
+      "name":"Tide"
+    },
+    {
+      "id":4,
+      "path":"./assets/Tide/Tide_4.png",
+      "name":"Tide"
+    },
+    {
+      "id":5,
+      "path":"./assets/Tide/Tide_5.png",
+      "name":"Tide"
+    },
+    {
+      "id":6,
+      "path":"./assets/Tide/Tide_6.jpg",
+      "name":"Tide"
+    },
+    {
+      "id":7,
+      "path":"./assets/Tide/Tide_7.PNG",
+      "name":"Tide"
+    },
+    {
+      "id":8,
+      "path":"./assets/Head_N_Sholder/HS_1.jpg",
+      "name":"Head & Shoulders"
+    },
+    {
+      "id":9,
+      "path":"./assets/Head_N_Sholder/HS_2.jpg",
+      "name":"Head & Shoulders"
+    },
+    {
+      "id":10,
+      "path":"./assets/Head_N_Sholder/HS_3.jpg",
+      "name":"Head & Shoulders"
+    },
+    {
+      "id":11,
+      "path":"./assets/Head_N_Sholder/HS_4.jpg",
+      "name":"Head & Shoulders"
+    },
+    {
+      "id":12,
+      "path":"./assets/Head_N_Sholder/HS_5.jpg",
+      "name":"Head & Shoulders"
+    },
+    {
+      "id":13,
+      "path":"./assets/Head_N_Sholder/HS_6.jpeg",
+      "name":"Head & Shoulders"
+    },
+    {
+      "id":14,
+      "path":"./assets/Head_N_Sholder/HS_7.jpeg",
+      "name":"Head & Shoulders"
+    },
+    {
+      "id":15,
+      "path":"./assets/Head_N_Sholder/HS_8.jpg",
+      "name":"Head & Shoulders"
+    },
+    {
+      "id":16,
+      "path":"./assets/Head_N_Sholder/HS_9.jpg",
+      "name":"Head & Shoulders"
+    }
+  ];
+  response : any = {
+    Details: "",
+    TransactionDate : "",
+    Items:"",
+    ItemCost : "",
+    Total : ""
+}
   constructor(
     private apiService: ApiService
   ) { }
@@ -65,22 +143,35 @@ export class InsPosComponent implements OnInit {
     let lastDotIdx = this.images.lastIndexOf(".");
     let filename = this.images.substring(lastSlashIdx + 1, lastDotIdx);
     console.log(filename);
-    this.apiService.getEstimatedPos({filename: filename}).then(result => {
-      console.log(result);
-      // if( result && result["Category"] && result["Probability"] ) {
-      //   let prob = result["Probability"];
-      //   this.response = {
-      //     Category: result["Category"],
-      //     Probability: (prob * 100).toFixed(2)
-      //   }
-      // } else {
-      //   this.response = {
-      //     Category: "",
-      //     Probability: ""
-      //   }
-      // }
+    this.apiService.getEstimatedPos({filename: filename}).then(result1 => {
+      console.log(result1);
+      var result= {"StoreDetails": [{"label": "MerchantName", "Description": "BIG BAZAAR (FUTURE RETAIL LTD)"}, {"label": "MerchantAddress", "Description": "GURU EMPIRES, GRAND ROAD PURI, NEAR TOWN POLICE STATION, PURI-752001"}, {"label": "MerchantPhoneNumber", "Description": "06752-252602"}], "DateTime": [{"label": "TransactionDate", "Description": "01.07.2017"}], "Items": [{"label": "ItemName1", "Description": " I CO HENLY, XL, NAVY", "ItemCost": "1049.00"}, {"label": "ItemName2", "Description": "DJ & C MNS SOCKS", "ItemCost": "99.00"}, {"label": "ItemName3", "Description": "BUF, FSTRS, 32,KHAKI", "ItemCost": "1399.00"}, {"label": "ItemName4", "Description": "FSB 1000 GV on 2500", "ItemCost": "0.01"}, {"label": "ItemName5", "Description": "SHOPPING CARRY BAG", "ItemCost": "5.00"}], "Total": [{"label": "Total"}, {"Description": "2552.01"}]}
+      if( result && result["StoreDetails"] && result["DateTime"] && result["Items"] && result["Total"] ) {
+          var Details= result["StoreDetails"].map(a => a.Description);
+          var TransactionDate = result["DateTime"].map(a => a.Description);
+          var Items= result["Items"].map(a => a.Description);
+          var ItemCost = result["Items"].map(a => a.ItemCost);
+          var Total = result["Total"].find(a => a.Description);
+        this.response = {
+           Details: Details,
+           TransactionDate:TransactionDate,
+           Items: Items,
+           ItemCost : ItemCost,
+           Total : Total.Description
+        }
+        console.log(this.response);
+
+      } else {
+        this.response = {
+          Details: "",
+          TransactionDate : "",
+          Items:"",
+          ItemCost : "",
+          Total : ""
+        }
+      }
     });
-    this.uploadedimage = this.selImgObj.path;
+    // this.uploadedimage = this.selImgObj.path;
   }
 
 }
