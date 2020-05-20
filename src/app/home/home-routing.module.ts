3d55333f3ef3876c25165df5af3233add0550962
rakesh.roshan <rakesh.roshan@affineanalytics.com>
2020-05-20 16:44:43 +0530

    Header routing is updated for Vibration Analysis.

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,14 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent }   from './home.component';
-// import { IntroComponent }   from './components/intro/intro.component';
-import { InstoreComponent }   from './components/instore/instore.component';
-import { InsTrafficComponent } from './components/ins-traffic/ins-traffic.component';
-import { InsPosComponent }   from './components/ins-pos/ins-pos.component';
-import { InsCounterfeitComponent }   from './components/ins-counterfeit/ins-counterfeit.component';
-import { InsReportComponent } from './components/ins-report/ins-report.component';
-import { ManVibrateComponent } from './components/man-vibrate/man-vibrate.component';
-import { ManVibrateMultiComponent } from './components/man-vibrate-multi/man-vibrate-multi.component';
 
 
 const routes: Routes = [
@@ -30,7 +22,6 @@ const routes: Routes = [
       { path: 'man-damage', loadChildren: () => import('./components/man-damage/man-damage.module').then(m => m.ManDamageModule) },
       { path: 'man-vibrate', loadChildren: () => import('./components/man-vibrate/man-vibrate.module').then(m => m.ManVibrateModule) },
       { path: 'man-vibrate-multi', loadChildren: () => import('./components/man-vibrate-multi/man-vibrate-multi.module').then(m => m.ManVibrateMultiModule) },
-
 
     ]
   }
